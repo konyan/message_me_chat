cb647029a01f6b0714b1931720a7a7a75f42f31b
konyan <devkonyan@gmail.com>
2020-03-14 01:20:03 +0630

    generate controller chatsroom and sessions

---
+++ app/controllers/chatsroom_controller.rb
@@ -0,0 +1,2 @@
+class ChatsroomController < ApplicationController
+end
---
+++ test/controllers/chatsroom_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ChatsroomControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
