8e6ee765283dcedafb02b09923a767d8fb2bbf36
konyan <devkonyan@gmail.com>
2020-03-14 01:57:49 +0630

    add navigation layout rails

--- Gemfile
+++ Gemfile
@@ -2,6 +2,9 @@ source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.5'
+#import gem
+gem 'semantic-ui-sass'
+gem 'jquery-rails'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,12 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
 //= require activestorage
 //= require turbolinks
+//= require semantic-ui
 //= require_tree .
+
+$(document).on("turbolinks:load", function() {
+  $(".ui.dropdown").dropdown();
+});
