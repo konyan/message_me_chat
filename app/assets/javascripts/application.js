b53df3a195539782eb3affe3cdc8038bda3a6c11
konyan <devkonyan@gmail.com>
2020-03-15 04:00:33 +0630

    custom msg display

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,6 +23,15 @@ scroll_bottom = function() {
   }
 };
 
+submit_message = function() {
+  $("#message_body").on("keydown", function(e) {
+    if (e.keyCode == 13) {
+      $("button").click();
+      e.target.value = "";
+    }
+  });
+};
+
 $(document).on("turbolinks:load", function() {
   $(".ui.dropdown").dropdown();
   $(".message .close").on("click", function() {
@@ -31,4 +40,5 @@ $(document).on("turbolinks:load", function() {
       .transition("fade");
   });
   scroll_bottom();
+  submit_message();
 });
--- app/controllers/chatsroom_controller.rb
+++ app/controllers/chatsroom_controller.rb
@@ -3,7 +3,7 @@ class ChatsroomController < ApplicationController
 
   def index
     @message = Message.new
-    @messages  = Message.all
+    @messages  = Message.custom_display
   end
 
 
